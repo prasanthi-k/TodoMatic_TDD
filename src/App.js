436ead0aba86927141d98cb04a1faa9340038d5a
prasanthi-k <46198559+prasanthi-k@users.noreply.github.com>
2022-03-31 23:11:27 +0530

    Update App.js with test Id

--- src/App.js
+++ src/App.js
@@ -95,7 +95,7 @@ function App(props) {
 
   return (
     <div className="todoapp stack-large">
-      <h1>TodoMatic</h1>
+      <h1 data-testid = "todo-heading">TodoMatic</h1>
       <Form addTask={addTask} />
 
       <div className="filters btn-group stack-exception">{filterList}</div>
